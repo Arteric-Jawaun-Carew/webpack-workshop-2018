ca88a3fbdb10e74fc838e40eefcb66ae09972dfc
Jawaun Carew <jawaun.carew@arteric.com>
2020-08-13 13:02:53 -0400

    Webpack from scratch section, exercises from 2nd video to tree shaking

---
+++ src/button.js
@@ -0,0 +1,10 @@
+//take a string, the button label and return a element
+
+
+const makeButton = (buttonName) => {
+    return `button ${buttonName}`;
+};
+
+
+// module.exports = makeButton;
+export default makeButton
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import nav from "./nav";
+import {top, bottom} from "./footer";
+import makeBtn from './button';
+import {blue, makeColorStyle, makeColor} from "./button-styles";
+
+console.log(nav(), top, bottom, makeBtn('Success'), makeColorStyle("cyan"));
